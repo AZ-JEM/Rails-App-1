ea5e7dfb4894642bf5026d4225799eb5faa30531
AZ-JEMgit <txazjem@gmail.com>
2018-04-23 17:13:05 -0700

    Clean up routes.rb : 180423

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'simple_pages#index'
   get 'simple_pages/about'
-
   get 'simple_pages/contact'
 
-  # I am assuming this is no longer necessary?
-  # get 'simple_pages/index'
-  root 'simple_pages#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
