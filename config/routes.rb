f57d68b15702af9c74f54cb8c8a15bd2f66582c7
AZ-JEMgit <txazjem@gmail.com>
2018-04-29 23:54:21 -0700

    Heroku animation bug attempt (a) : 180429

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#index'
+  get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  
+  root 'simple_pages#index'
 
 end
