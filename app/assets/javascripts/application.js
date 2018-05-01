699fee01af01a3484a1b88aed662cec33312ebab
AZ-JEMgit <txazjem@gmail.com>
2018-05-01 04:57:32 -0700

    View transition animations, selected anchor indication and tooltips : 180501

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application.js, which will include all thee files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -14,3 +14,52 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+
+// ====================================================================== /
+//  Turbolinks 5 event structure (chronological)
+// ====================================================================== /
+/*
+document.addEventListener('turbolinks:click', function() {
+  // var x = document.getElementById('view-container');
+  // x.classList.toggle('exit');
+  // console.log('*click');
+});
+
+document.addEventListener('turbolinks:before-visit', function() {
+  // console.log('*before-visit');
+});
+
+document.addEventListener('turbolinks:visit', function() {
+  // console.log('*visit');
+});
+
+document.addEventListener('turbolinks:request-start', function() {
+  // console.log('*request-start');
+});
+
+document.addEventListener('turbolinks:request-end', function() {
+  // console.log('*request-end');
+});
+
+document.addEventListener('turbolinks:before-cache', function() {
+  // console.log('*before-cache');
+});
+
+document.addEventListener('turbolinks:before-render', function() {
+  // console.log('*before-render');
+});
+
+document.addEventListener('turbolinks:render', function() {
+  // console.log('*render');
+});
+*/
+
+$(document).on('turbolinks:load', function() {
+  // enable tooltips :
+  $('[toggle="tooltip"]').tooltip();
+  //  apply fade-in animation :
+  $('#view-container').toggleClass('enter');
+});
+
+// document.addEventListener('turbolinks:load', function() {
+// document.getElementById('view-container').classList.toggle('enter');
