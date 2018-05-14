f7703edd74a8523dc570d25e86acd1acd373c3a2
AZ-JEMgit <txazjem@gmail.com>
2018-05-13 17:48:01 -0700

    Exercise 4.3

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,11 @@
 class SimplePagesController < ApplicationController
+
   def index
   end
+
+  # Exercise 4.3
+  def landing_page
+    @featured_product = Product.first
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'simple_pages/index'
-  root 'simple_pages#index'
+
+  # root 'simple_pages#index'
+  root 'simple_pages#landing_page'
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
+
   resources :products
 
+  # Exercise 4.3
+  resources :orders, only: [:index, :show, :create, :destroy]
 
 end
