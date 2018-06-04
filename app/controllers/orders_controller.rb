217c545e3e824d051dc68c926daf2c56504552f5
AZ-JEMgit <txazjem@gmail.com>
2018-06-03 18:15:18 -0700

    Exercise 4.6

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,14 @@
 class OrdersController < ApplicationController
 
   def index
+    # Exercise 4.6
+    # @orders = Order.all
+    @orders = Order.includes(:product).all
   end
 
   def show
+    # Exercise 4.6
+    @order = Order.find(params[:id])
   end
 
   def new
