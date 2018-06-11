16a09600701b1ddf149a1fac851dbfa3d86a0ace
AZ-JEMgit <txazjem@gmail.com>
2018-06-11 04:30:04 -0700

    Clean and prioritize routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'simple_pages/index'
 
-  # root 'simple_pages#index'
-
-  get '/demo', :to => redirect('pages/demo.html')
-
-  # get 'simple_pages/demo'
-  get 'simple_pages/landing_page'
-
-  resources :products
-
-  # Exercise 4.3
-  resources :orders, only: [:index, :show, :create, :destroy]
-
   root 'simple_pages#landing_page'
+  resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+  get '/demo', to: redirect('pages/demo.html')
 
 end
