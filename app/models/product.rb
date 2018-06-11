ba1f1d921fe37e8821007ec269e0874a4a636e0e
AZ-JEMgit <txazjem@gmail.com>
2018-06-11 05:19:15 -0700

    Product field validators

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :orders
+
+  validates :name, presence: true, length: { in: 3..20 }
+  validates :description, presence: true
+  validates :price, presence: true
+
 end
