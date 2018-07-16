8cfc0d5a6365b49a43f4666023968f4f5a02ab62
Jeff McCarley <txazjem@gmail.com>
2018-07-15 18:16:04 -0700

    Auth initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # Authentication
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'simple_pages/index'
 
+  # Authentication
+  devise_for :users
+
   # ---------------------------------------------------------------------- /
   # Exercise 5.1
   # ---------------------------------------------------------------------- /
@@ -10,18 +13,18 @@ Rails.application.routes.draw do
   #   No significant changes from 4.7
   # ---------------------------------------------------------------------- /
 
-  # root 'simple_pages#landing_page'
-  root to: redirect('pages/demo.html')
+  root 'simple_pages#landing_page'
+  # root to: redirect('pages/demo.html')
 
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
 
-  # get '/demo', to: redirect('pages/demo.html')
-  get '/home', to: 'simple_pages#landing_page'
+  get '/demo', to: redirect('pages/demo.html')
+  # get '/home', to: 'simple_pages#landing_page'
 
   # Exercise 5.3
   get '/contact', to: 'simple_pages#contact'
 
   post 'simple_pages/thank_you'
-  
+
 end
