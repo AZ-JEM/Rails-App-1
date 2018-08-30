3e9b23a17cd4604268f16cd6aeecec2dde604979
Jeff McCarley <txazjem@gmail.com>
2018-08-30 04:46:10 -0700

    Exercise 6.6 (primitive rescue)

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,11 +7,11 @@ class PaymentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @user = current_user
 
-    puts '*' * 100
-    puts @user.id
-    puts @product.id
-    puts @product.price
-    puts '*' * 100
+    # puts '*' * 100
+    # puts @user.id
+    # puts @product.id
+    # puts @product.price
+    # puts '*' * 100
 
     token = params[:stripeToken]
 
@@ -28,18 +28,46 @@ class PaymentsController < ApplicationController
       # create new order...
       if charge.paid
         Order.create(user_id: @user.id, product_id: @product.id, total: @product.price)
+        flash.notice = "Thank you for your purchase!"
       end
 
     # Handle errors...
+    # The card has been declined
     rescue Stripe::CardError => e
-      # The card has been declined
-      body = e.json_body
-      err = body[:error]
-      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    body = e.json_body
+    err = body[:error]
+    flash.alert = "Error processing payment: #{err[:message]}"
+
+    # Too many requests made to the API too quickly
+    rescue Stripe::RateLimitError => e
+    flash.alert = "Request limit"
+
+    # Invalid parameters were supplied to Stripe's API
+    rescue Stripe::InvalidRequestError => e
+    flash.alert = "Bad parameters"
+
+    # Authentication with Stripe's API failed
+    # (maybe you changed API keys recently)
+    rescue Stripe::AuthenticationError => e
+    flash.alert = "Authentication failure"
+
+    # Network communication with Stripe failed
+    rescue Stripe::APIConnectionError => e
+    flash.alert = "Communication failure"
+
+    # Display a very generic error to the user, and maybe send
+    # yourself an email
+    rescue Stripe::StripeError => e
+    flash.alert = "Generic error"
+
+    # Something else happened, completely unrelated to Stripe
+    rescue => e
+    flash.alert = "Unknown error"
+
     end
 
-    # Success...
-    redirect_to product_path(@product), notice: 'Thank you for your purchase!'
+    # return to product page...
+    redirect_to product_path(@product)
 
   end
 
