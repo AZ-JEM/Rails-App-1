d80bf1ad35f2ebd3130ea6c338440bbe1065e329
Jeff McCarley <txazjem@gmail.com>
2018-09-01 01:13:12 -0700

    Clean-up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,33 @@
 Rails.application.routes.draw do
 
-  # Exercise 6.6 (requires POST)
-  # "get" from generator...
-  post 'payments/create'
-
-  get '/users', to: 'users#index'
-  # get 'users/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get 'simple_pages/index'
-
   # Home      --------------------------------------------------/
   root 'simple_pages#landing_page'
 
   # Users     --------------------------------------------------/
   devise_for :users, controllers: { registrations: "user_registrations" }
 
-  post 'simple_pages/thank_you'
-
-  get '/contact', to: 'simple_pages#contact'
-
   # Products  --------------------------------------------------/
   resources :products do
     resources :comments
   end
-  # admin
-  get '/table', to: 'products#table'
 
   # Orders    --------------------------------------------------/
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # Demo      --------------------------------------------------/
   get '/demo', to: redirect('pages/demo.html')
+
+  # Response ---------------------------------------------------/
+  post 'simple_pages/thank_you'
+
+  # Contact ---------------------------------------------------/
+  get '/contact', to: 'simple_pages#contact'
+
+  # Stripe    --------------------------------------------------/
+  post 'payments/create'
+
+  # Admin     --------------------------------------------------/
+  get '/table', to: 'products#table'
+  get '/users', to: 'users#index'
 
 end
